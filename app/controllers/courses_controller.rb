c2c81f19822b4f39eda4190a49d2776344f2d1e1
Alfredo Peña <aalfredoplz@gmail.com>
2021-08-22 10:07:28 -0400

    authenticate user

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
   # GET /courses or /courses.json
   def index
     @courses = Course.all
--- db/migrate/20210822133930_create_join_table_courses_students.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCoursesStudents < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :users, :courses do |t|
-      # t.index [:user_id, :course_id]
-      # t.index [:course_id, :user_id]
-    end
-  end
-end
