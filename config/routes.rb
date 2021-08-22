a4f2767913450bf51295ac259e9da913a2569585
Alfredo Peña <aalfredoplz@gmail.com>
2021-08-22 09:59:50 -0400

    controller dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'dashboard/home'
   devise_for :users
   resources :courses
   resources :students
+
+  root 'dashboard#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
