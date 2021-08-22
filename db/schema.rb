f4d81c725bf5b6e7858830cfc8366927d21cebf5
Alfredo Peña <aalfredoplz@gmail.com>
2021-08-21 23:58:39 -0400

    model students courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_023235) do
+ActiveRecord::Schema.define(version: 2021_08_22_034925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,4 +33,15 @@ ActiveRecord::Schema.define(version: 2021_08_22_023235) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "students_courses", force: :cascade do |t|
+    t.bigint "student_id", null: false
+    t.bigint "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_students_courses_on_course_id"
+    t.index ["student_id"], name: "index_students_courses_on_student_id"
+  end
+
+  add_foreign_key "students_courses", "courses"
+  add_foreign_key "students_courses", "students"
 end
