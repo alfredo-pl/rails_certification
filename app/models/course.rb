7095f9faab843f496f3ac29d22ac85fcc65b34ac
Alfredo Peña <aalfredoplz@gmail.com>
2021-08-21 23:59:15 -0400

    cardinality

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+    has_many :students_courses
+    has_many :students, through: :students_courses
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,4 @@
 class Student < ApplicationRecord
+    has_many :students_courses
+    has_many :courses , through: :students_courses
 end
