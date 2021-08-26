a3842ede5cdb407d9290a5cdc0327aa6fd6c99c6
Alfredo Peña <aalfredoplz@gmail.com>
2021-08-25 21:24:09 -0400

    params run to student

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
   def home
+    @existe = true
+   if params[:run].present?
+    @student = Student.where("run = ?", params[:run])
+
+    if @student.empty?
+      @existe = false
+    end
   end
+end
 end
