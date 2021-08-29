1bad6d7c039bd47f6c816de84df170f4f5b73a64
Alfredo Peña <aalfredoplz@gmail.com>
2021-08-29 10:29:16 -0400

    route certification pdf

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,39 @@
 class DashboardController < ApplicationController
+before_action :set_student, only: %i[ student_certification]
   def home
     @existe = true
-   if params[:run].present?
-    @student = Student.where("run = ?", params[:run])
-
-    if @student.empty?
-      @existe = false
+    if params[:run].present?
+      @student = Student.where("run = ?", params[:run])
+      if @student.empty?
+        @existe = false
+      end
     end
   end
-end
+
+  def student_certification
+    if !@student.empty? || !@stundet.nil?
+     
+      respond_to do |format|
+        format.pdf do 
+          render pdf: "Certificado",
+            template: 'dashboard/pdf',
+            page_size: 'A4',
+            margin:  {   top: 10,
+              bottom: 10,
+              left: 20,
+              right: 20}
+        end
+      end
+    else
+      respond_to do |format|
+        format.pdf { redirect_to root_path, alert: "Error al cargar pdf verifique su run." }
+      end
+    end  
+  end
+  
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_student
+    @student = Student.where("run = ?", params[:run])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'dashboard/home', to: 'dashboard#home'
+  get 'certification/:run' , to: 'dashboard#student_certification', as:'certification', :defaults=>{ :format => 'pdf'}
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
